07d6a593f4c2d256e4dd20e3bc0003959e07ac59
AminSamir83 <37070162+AminSamir83@users.noreply.github.com>
2018-11-07 10:41:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HP
+ * Date: 05/11/2018
+ * Time: 15:47
+ */
+$nom='Sam';
+$anneeNaissance=2000;
+echo "Bonjour ".$nom." vous avez ".(string) (2018-$anneeNaissance)." ans ."
+?>
